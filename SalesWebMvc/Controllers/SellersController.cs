a669ee6322271a12e94c9c4d2f328913b72e7d68
Inhangá <nakappa1@hotmail.com>
2019-12-18 01:21:53 -0300

    Tela simples de cadastro.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //anotation referente a post
+        [ValidateAntiForgeryToken] //serve para prevenção de ataque CSRF(ataques que se aproveitam sessões de autenticações para enviar dados maliciosos)
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
